527aa72d58b4344e5beb4492023a6b3c2a877417
Jacob Adams <tookmund@gmail.com>
2015-08-10 17:46:38 -0400

    cc.mk: create

---
+++ cc.mk
@@ -0,0 +1,13 @@
+# C Compiler Check
+# From Maker
+# https://github.com/Tookmund/Maker
+CC= cc
+cc.lock:
+	touch cc.lock
+
+cc: cc.lock
+	echo 'int main () { return 0; }' > cc.c
+	${CC} cc.c -o cc
+	./cc
+	rm -f cc.c
+
